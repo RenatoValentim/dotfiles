347a63e5ea1578073bc84b3ca9aa6ea8098f5283
rvjs <renato.valentim@tempest.com.br>
2023-01-15 18:57:23 -0300

    refactor: add telescope and git keymaps

--- config/nvim/lua/user/personal/keymaps.lua
+++ config/nvim/lua/user/personal/keymaps.lua
@@ -4,59 +4,67 @@ local opts = { noremap = true, silent = true }
 --Remap space as leader key
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
-keymap.set("n", "<Space>", "<Nop>")
+keymap.set("n", "<Space>", "<Nop>", opts)
 
-keymap.set("n", "<leader>w", ":w<Return>")
-keymap.set("n", "<leader>W", ":wa<Return>")
-keymap.set("n", "<leader>q", ":q<Return>")
-keymap.set("n", "<leader>Q", ":q!<Return>")
-keymap.set("n", "<leader>x", ":x<Return>")
-keymap.set("n", "<leader>ue", ":e ")
-keymap.set("n", "<leader>uf", ":find ")
-keymap.set("n", "<leader>uc", ":! ")
-keymap.set("n", "<leader>ur", ":luafile %<Return>")
-keymap.set("n", "<leader>d", "dd")
-keymap.set("n", "<leader>uh", ":nohlsearch<Return>")
-keymap.set("n", "<leader>Nc", ":e ~/.config/nvim/init.lua<Return>")
-keymap.set("n", "<leader>Nl", ":luafile %<Return>")
-keymap.set("n", "<C-f>", ":%s///gc")
-keymap.set("n", "<C-f>f", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
+keymap.set("n", "<leader>w", ":w<Return>", opts)
+keymap.set("n", "<leader>W", ":wa<Return>", opts)
+keymap.set("n", "<leader>q", ":q<Return>", opts)
+keymap.set("n", "<leader>Q", ":q!<Return>", opts)
+keymap.set("n", "<leader>x", ":x<Return>", opts)
+keymap.set("n", "<leader>ue", ":e ", opts)
+keymap.set("n", "<leader>uf", ":find ", opts)
+keymap.set("n", "<leader>uc", ":! ", opts)
+keymap.set("n", "<leader>ur", ":luafile %<Return>", opts)
+keymap.set("n", "<leader>d", "dd", opts)
+keymap.set("n", "<leader>uh", ":nohlsearch<Return>", opts)
+keymap.set("n", "<leader>Nc", ":e ~/.config/nvim/init.lua<Return>", opts)
+keymap.set("n", "<leader>Nl", ":luafile %<Return>", opts)
+keymap.set("n", "<C-f>", ":%s///gc", opts)
+keymap.set("n", "<C-f>f", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]], opts)
 
-keymap.set("n", "x", '"_x')
+keymap.set("n", "x", '"_x', opts)
 
 -- Increment/decrement
-keymap.set("n", "+", "<C-a>")
-keymap.set("n", "-", "<C-x>")
+keymap.set("n", "+", "<C-a>", opts)
+keymap.set("n", "-", "<C-x>", opts)
 
 -- Select all
-keymap.set("n", "<C-a>", "gg<S-v>G")
+keymap.set("n", "<C-a>", "gg<S-v>G", opts)
 
 -- Split window
-keymap.set("n", "ss", ":split<Return><C-w>w")
-keymap.set("n", "vs", ":vsplit<Return><C-w>w")
+keymap.set("n", "ss", ":split<Return><C-w>w", opts)
+keymap.set("n", "vs", ":vsplit<Return><C-w>w", opts)
 
 -- Move window
-keymap.set("n", "<C-h>", "<C-w>h")
-keymap.set("n", "<C-j>", "<C-w>j")
-keymap.set("n", "<C-k>", "<C-w>k")
-keymap.set("n", "<C-l>", "<C-w>l")
+keymap.set("n", "<C-h>", "<C-w>h", opts)
+keymap.set("n", "<C-j>", "<C-w>j", opts)
+keymap.set("n", "<C-k>", "<C-w>k", opts)
+keymap.set("n", "<C-l>", "<C-w>l", opts)
 
 -- Resize window
-keymap.set("n", "<A-left>", "<C-w><")
-keymap.set("n", "<A-right>", "<C-w>>")
-keymap.set("n", "<A-up>", "<C-w>+")
-keymap.set("n", "<A-down>", "<C-w>-")
+keymap.set("n", "<A-left>", "<C-w><", opts)
+keymap.set("n", "<A-right>", "<C-w>>", opts)
+keymap.set("n", "<A-up>", "<C-w>+", opts)
+keymap.set("n", "<A-down>", "<C-w>-", opts)
 
 -- Move current line / block with Alt-j/k a la vscode.
 -- Move text up and down
-keymap.set("n", "<A-k>", ":move .-2<Return>==")
-keymap.set("n", "<A-j>", ":move .+1<Return>==")
-keymap.set("v", "<A-k>", ":move '<-2<Return>gv=gv")
-keymap.set("v", "<A-j>", ":move '>+1<Return>gv=gv")
-keymap.set("i", "<A-k>", "<Esc>:move .-2<Return>==gi")
-keymap.set("i", "<A-j>", "<Esc>:move .+1<Return>==gi")
+keymap.set("n", "<A-k>", ":move .-2<Return>==", opts)
+keymap.set("n", "<A-j>", ":move .+1<Return>==", opts)
+keymap.set("v", "<A-k>", ":move '<-2<Return>gv=gv", opts)
+keymap.set("v", "<A-j>", ":move '>+1<Return>gv=gv", opts)
+keymap.set("i", "<A-k>", "<Esc>:move .-2<Return>==gi", opts)
+keymap.set("i", "<A-j>", "<Esc>:move .+1<Return>==gi", opts)
 
-keymap.set("v", "<", "<gv")
-keymap.set("v", ">", ">gv")
+keymap.set("v", "<", "<gv", opts)
+keymap.set("v", ">", ">gv", opts)
 
-keymap.set("i", "jk", "<ESC>")
+keymap.set("i", "jk", "<ESC>", opts)
+
+-- Telescope
+keymap.set("n", "<leader>ff", ":Telescope find_files<CR>", opts)
+keymap.set("n", "<leader>ft", ":Telescope live_grep<CR>", opts)
+keymap.set("n", "<leader>fb", ":Telescope buffers<CR>", opts)
+
+-- -- Git
+keymap.set("n", "<leader>gg", "<cmd>lua _LAZYGIT_TOGGLE()<CR>", opts)
