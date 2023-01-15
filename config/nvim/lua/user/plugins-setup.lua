81c908fe8cb00efcdc17715f53284f3bec80e468
rvjs <renato.valentim@tempest.com.br>
2023-01-15 05:58:11 -0300

    feat: add color scheme

--- config/nvim/lua/user/plugins-setup.lua
+++ config/nvim/lua/user/plugins-setup.lua
@@ -57,5 +57,8 @@ return packer.startup(function(use)
   -- snippets
   use { "L3MON4D3/LuaSnip", commit = "8f8d493e7836f2697df878ef9c128337cbf2bb84" } --snippet engine
   use { "rafamadriz/friendly-snippets", commit = "2be79d8a9b03d4175ba6b3d14b082680de1b31b1" } -- a bunch of snippets to use
+
+  -- Colorschemes
+  use { "folke/tokyonight.nvim", commit = "66bfc2e8f754869c7b651f3f47a2ee56ae557764" }
 end)
 
