0490c52ca0ab896d84955e5f18e7e59d8a253904
rvjs <renato.valentim@tempest.com.br>
2023-01-16 16:45:41 -0300

    feat: add which-key and todo-comments

---
+++ config/nvim/lua/user/todo-comments.lua
@@ -0,0 +1,24 @@
+local status_ok, todo_comments = pcall(require, "todo-comments")
+if not status_ok then
+  return
+end
+
+todo_comments.setup {
+  keywords = {
+    FIX = {
+      icon = " ", -- icon used for the sign, and in search results
+      color = "#DC2626", -- can be a hex color, or a named color (see below)
+      alt = { "FIXME", "BUG", "FIXIT", "ISSUE", "ERROR", "ERR" }, -- a set of other keywords that all map to this FIX keywords
+      -- signs = false, -- configure signs for some keywords individually
+    },
+    TODO = { icon = " ", color = "#2563EB", },
+    HACK = { icon = " ", color = "#FBBF24" },
+    WARN = { icon = " ", color = "#FBBF24", alt = { "WARNING", "XXX" } },
+    PERF = { icon = " ", alt = { "OPTIM", "PERFORMANCE", "OPTIMIZE" } },
+    NOTE = { icon = " ", color = "#10B981", alt = { "INFO" } },
+  },
+}
+
+vim.keymap.set("n", "<leader>tqf", ":TodoQuickFix<Return>", {})
+vim.keymap.set("n", "<leader>tll", ":TodoLocList<Return>", {})
+vim.keymap.set("n", "<leader>tt", ":TodoTelescope<Return>", {})
