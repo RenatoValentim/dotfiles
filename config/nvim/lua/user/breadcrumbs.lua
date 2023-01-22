a0fac7afd3df61f9969fba0471f8695d09be3b05
rvjs <renato.valentim@tempest.com.br>
2023-01-22 02:00:34 -0300

    refactor: add protected call on require of the lspsaga

--- config/nvim/lua/user/breadcrumbs.lua
+++ config/nvim/lua/user/breadcrumbs.lua
@@ -1,4 +1,9 @@
-require("lspsaga").setup {
+local status_ok, lspsaga = pcall(require, "lspsaga")
+if not status_ok then
+  return
+end
+
+lspsaga.setup {
   symbol_in_winbar = {
     enable = true,
     separator = "  ",
