5be1386cc54c36f372d925fdfdd3376718c1efd0
rvjs <renato.valentim@tempest.com.br>
2023-03-29 15:52:38 -0300

    refactor: update tree-sitter

--- config/nvim/lua/user/plugins-setup.lua
+++ config/nvim/lua/user/plugins-setup.lua
@@ -144,7 +144,7 @@ return packer.startup(function(use)
   -- Treesitter
   use {
     "nvim-treesitter/nvim-treesitter",
-    commit = "8e763332b7bf7b3a426fd8707b7f5aa85823a5ac",
+    commit = "0a907316fad22b4495541328bc1a92378baf1b68",
   }
 
   -- Automatically set up your configuration after cloning packer.nvim
