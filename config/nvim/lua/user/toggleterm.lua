be6bee555c8641ca69f5041e51c72d093008826d
rvjs <renato.valentim@tempest.com.br>
2023-05-12 03:54:54 -0300

    refactor: change height to lazygit terminal

--- config/nvim/lua/user/toggleterm.lua
+++ config/nvim/lua/user/toggleterm.lua
@@ -46,7 +46,7 @@ function M.config()
     float_opts = {
       border = "none",
       width = 100000,
-      height = 50,
+      height = 51,
     },
     on_open = function(_)
       vim.cmd "startinsert!"
