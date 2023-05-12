7d455cf407ec3b7c3325e6e1cb87beeaabfc77d2
rvjs <renato.valentim@tempest.com.br>
2023-05-12 04:25:11 -0300

    fix: rollback commit to vim-visual-multi

--- config/nvim/lua/user/vim-visual-multi.lua
+++ config/nvim/lua/user/vim-visual-multi.lua
@@ -1,10 +1,7 @@
 local M = {
   "mg979/vim-visual-multi",
-  branch = "main",
+  commit = "488daad47c6db86158501669bf7158a398f59620",
+  branch = "master",
 }
-
-function M.config()
-  require("vim-visual-multi").setup()
-end
 
 return M
