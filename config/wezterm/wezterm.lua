6b11fdd7837131d005c6ab6a5c3a88e28073cbf0
rvjs <renato.valentim@tempest.com.br>
2023-05-18 14:17:23 -0300

    refactor: update wezterm

--- config/wezterm/wezterm.lua
+++ config/wezterm/wezterm.lua
@@ -8,7 +8,8 @@ end
 
 config.color_scheme = "tokyonight_night"
 config.enable_tab_bar = false
-config.font = wezterm.font 'Fira Code'
+config.font = wezterm.font 'FiraCode Nerd Font'
+config.warn_about_missing_glyphs = false
 config.font = wezterm.font("JetBrains Mono", {
 	weight = "Regular", -- "Regular" "Thin" "ExtraLight" "Light" "DemiLight" "Book" "Regular" "Medium" "DemiBold" "Bold" "ExtraBold" "Black" "ExtraBlack"
 	italic = true,
