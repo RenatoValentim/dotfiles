914e7d014951dba9434040991973bc0a05b25026
rvjs <renato.valentim@tempest.com.br>
2024-01-01 23:56:35 -0300

    refactor: update layout priority

--- config/awesome/personal/layout.lua
+++ config/awesome/personal/layout.lua
@@ -4,8 +4,8 @@ local awful = require("awful")
 -- Table of layouts to cover with awful.layout.inc, order matters.
 awful.layout.layouts = {
 	-- awful.layout.suit.floating,
-  awful.layout.suit.tile.left,
 	awful.layout.suit.tile,
+  awful.layout.suit.tile.left,
   awful.layout.suit.fair,
   awful.layout.suit.fair.horizontal,
 	-- awful.layout.suit.tile.bottom,
