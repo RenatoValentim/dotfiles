b6cfa4481f4595925fdec1617f04fe77e2998ec2
rvjs <renato.valentim@tempest.com.br>
2024-01-05 10:47:46 -0300

    refactor(nvim): update dapui

--- config/nvim/lua/user/dapui.lua
+++ config/nvim/lua/user/dapui.lua
@@ -26,18 +26,22 @@ function M.config()
     layouts = {
       {
         elements = {
-          { id = "scopes", size = 0.33 },
-          { id = "breakpoints", size = 0.17 },
-          { id = "stacks", size = 0.25 },
-          { id = "watches", size = 0.25 },
+          -- { id = "scopes", size = 0.33 },
+          { id = "scopes", size = 0.41 },
+          -- { id = "breakpoints", size = 0.17 },
+          { id = "breakpoints", size = 0.25 },
+          -- { id = "stacks", size = 0.25 },
+          -- { id = "watches", size = 0.25 },
+          { id = "watches", size = 0.33 },
         },
         size = 0.33,
-        position = "right",
+        position = "left",
       },
       {
         elements = {
-          { id = "repl", size = 0.45 },
-          { id = "console", size = 0.55 },
+          -- { id = "repl", size = 0.45 },
+          { id = "repl", size = 1.10 },
+          -- { id = "console", size = 0.55 },
         },
         size = 0.27,
         position = "bottom",
