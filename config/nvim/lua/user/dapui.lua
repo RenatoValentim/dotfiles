13c334af9a3b5e4c98ed994b21c4006f75de5ec9
RenatoValentim <rvrento28@gmail.com>
2024-02-01 11:38:47 -0300

    refactor(nvim): update dapui icons

--- config/nvim/lua/user/dapui.lua
+++ config/nvim/lua/user/dapui.lua
@@ -13,7 +13,23 @@ local M = {
 function M.config()
   require("dapui").setup {
     expand_lines = true,
-    icons = { expanded = "", collapsed = "", circular = "" },
+    icons = {
+      expanded = "",
+      collapsed = "",
+      circular = "",
+    },
+    controls = {
+      icons = {
+        pause = "",
+        play = "",
+        run_last = "",
+        step_back = "",
+        step_out = "",
+        step_into = "",
+        step_over = "",
+        terminate = "",
+      }
+    },
     mappings = {
       -- Use a table to apply multiple mappings
       expand = { "<CR>", "<2-LeftMouse>" },
@@ -27,12 +43,12 @@ function M.config()
       {
         elements = {
           -- { id = "scopes", size = 0.33 },
-          { id = "scopes", size = 0.41 },
+          { id = "scopes",      size = 0.41 },
           -- { id = "breakpoints", size = 0.17 },
           { id = "breakpoints", size = 0.25 },
           -- { id = "stacks", size = 0.25 },
           -- { id = "watches", size = 0.25 },
-          { id = "watches", size = 0.33 },
+          { id = "watches",     size = 0.33 },
         },
         size = 0.33,
         position = "left",
@@ -49,7 +65,7 @@ function M.config()
     },
     floating = {
       max_height = 0.9,
-      max_width = 0.5, -- Floats will be treated as percentage of your screen.
+      max_width = 0.5,             -- Floats will be treated as percentage of your screen.
       border = vim.g.border_chars, -- Border style. Can be 'single', 'double' or 'rounded'
       mappings = {
         close = { "q", "<Esc>" },
