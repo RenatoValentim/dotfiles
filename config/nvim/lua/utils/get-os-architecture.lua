67c6c194665d474088dcb0786ed4d284e81c79f5
RenatoValentim <rvrento28@gmail.com>
2024-02-06 07:23:54 -0300

    fix(nvim): remove new line character from get-os-architecture

--- config/nvim/lua/utils/get-os-architecture.lua
+++ config/nvim/lua/utils/get-os-architecture.lua
@@ -4,9 +4,8 @@ M.android_arch = "aarch64"
 
 function M.getLinuxArchitecture()
   local f = assert(io.popen("uname -m", "r"))
-  local arch = f:read "*a"
+  local arch = f:read("*a"):gsub("[\r\n]+", "")
   f:close()
-  ARCH = arch
   return arch
 end
 
