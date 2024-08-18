306753edf1d7f0dd842614808109a2dee1c5d9d4
rvjs <renato.valentim@tempest.com.br>
2024-08-18 17:30:46 -0300

    refactor: user snake case

--- config/lazyvim/lua/config/keymaps.lua
+++ config/lazyvim/lua/config/keymaps.lua
@@ -4,28 +4,27 @@
 
 local map = LazyVim.safe_keymap_set
 
-local function getKeymapOptions(noremap, silent, desc)
+local function get_keymap_options(noremap, silent, desc)
   return { noremap = noremap, silent = silent, desc = desc }
 end
 
 -- Dev
-map('n', '<leader>mw', ':w<Return>', getKeymapOptions(true, true, 'Save Current File'))
-map('n', '<leader>mq', ':q<Return>', getKeymapOptions(true, true, 'Quit Current File'))
-map('n', '<leader>mQ', ':q!<Return>', getKeymapOptions(true, true, 'Force Quit Current File'))
-map('n', '<leader>mx', ':x<Return>', getKeymapOptions(true, true, 'Save Quit Current File'))
-map('n', '<leader>mh', ':nohlsearch<Return>', getKeymapOptions(true, true, 'Turn Highlight Off'))
-map('n', '<leader>mN', ':e $MYVIMRC <CR>', getKeymapOptions(true, true, 'Open Neovim Config'))
+map('n', '<leader>mq', ':q<Return>', get_keymap_options(true, true, 'Quit Current File'))
+map('n', '<leader>mQ', ':q!<Return>', get_keymap_options(true, true, 'Force Quit Current File'))
+map('n', '<leader>mx', ':x<Return>', get_keymap_options(true, true, 'Save Quit Current File'))
+map('n', '<leader>mh', ':nohlsearch<Return>', get_keymap_options(true, true, 'Turn Highlight Off'))
+map('n', '<leader>mN', ':e $MYVIMRC <CR>', get_keymap_options(true, true, 'Open Neovim Config'))
 map('n', '<leader>mzi', '<c-w>_ | <c-w>|')
 map('n', '<leader>mzo', '<c-w>=')
 
 -- Increment/decrement
-map('n', '+', '<C-a>', getKeymapOptions(true, true, 'Increment Number'))
-map('n', '-', '<C-x>', getKeymapOptions(true, true, 'Decrement Number'))
+map('n', '+', '<C-a>', get_keymap_options(true, true, 'Increment Number'))
+map('n', '-', '<C-x>', get_keymap_options(true, true, 'Decrement Number'))
 
 -- Move cursor on insert mode
-map('i', '<C-l>', '<right>', getKeymapOptions(true, true, 'Insert Mode Mode Cursor Right'))
-map('i', '<C-h>', '<left>', getKeymapOptions(true, true, 'Insert Mode Mode Cursor Left'))
-map('i', '<C-j>', '<down>', getKeymapOptions(true, true, 'Insert Mode Mode Cursor Down'))
-map('i', '<C-k>', '<up>', getKeymapOptions(true, true, 'Insert Mode Mode Cursor Up'))
+map('i', '<C-l>', '<right>', get_keymap_options(true, true, 'Insert Mode Mode Cursor Right'))
+map('i', '<C-h>', '<left>', get_keymap_options(true, true, 'Insert Mode Mode Cursor Left'))
+map('i', '<C-j>', '<down>', get_keymap_options(true, true, 'Insert Mode Mode Cursor Down'))
+map('i', '<C-k>', '<up>', get_keymap_options(true, true, 'Insert Mode Mode Cursor Up'))
 
 map('i', 'jk', '<ESC>')
