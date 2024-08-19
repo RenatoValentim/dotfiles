4d72455468e4524c4eecc98849ff3ba6e475b0e2
rvjs <renato.valentim@tempest.com.br>
2024-08-19 15:03:12 -0300

    refactor: use lazy vim sage key map

--- config/lazyvim/lua/plugins/nvim-emment.lua
+++ config/lazyvim/lua/plugins/nvim-emment.lua
@@ -1,6 +1,8 @@
 return {
   'olrtg/nvim-emmet',
   config = function()
-    vim.keymap.set({ 'n', 'v' }, '<leader>xe', require('nvim-emmet').wrap_with_abbreviation)
+    local map = LazyVim.safe_keymap_set
+
+    map({ 'n', 'v' }, '<leader>xe', require('nvim-emmet').wrap_with_abbreviation)
   end,
 }
