c9f871cee1c412fe817ab88b514215b01da867b8
rvjs <renato.valentim@tempest.com.br>
2024-08-19 16:24:48 -0300

    refactor: don't use leader to commands

--- config/lazyvim/lua/config/keymaps.lua
+++ config/lazyvim/lua/config/keymaps.lua
@@ -7,9 +7,10 @@ local map = LazyVim.safe_keymap_set
 local key_options = require('me.utils.keymap_options_config').set_keymap_options
 
 -- Dev
+map('n', '<C-s>', ':wall<Return>', key_options({ desc = 'Save All' }))
 map('n', '<C-q>', ':q<Return>', key_options({ desc = 'Quit Current File' }))
 map('n', '<A-n>', ':e $MYVIMRC <CR>', key_options({ desc = 'Open Neovim Config' }))
-map('n', '<C-x>', ':x<Return>', key_options({ desc = 'Save Quit Current File' }))
+map('n', '<A-x>', ':x<Return>', key_options({ desc = 'Save Quit Current File' }))
 map('n', '<leader>zi', '<c-w>_ | <c-w>|')
 map('n', '<leader>zo', '<c-w>=')
 map('n', '<leader>h', ':nohlsearch<Return>', key_options({ desc = 'Turn Off Highlight' }))
