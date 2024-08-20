a99f07456615d63f2a25c0bc73ac17d386186284
rvjs <renato.valentim@tempest.com.br>
2024-08-20 13:41:16 -0300

    feat: added oil plugin

---
+++ config/lazyvim/lua/plugins/oil-nvim.lua
@@ -0,0 +1,126 @@
+local key_options = require('me.utils.keymap_options_config').set_keymap_options
+
+local M = {
+  'stevearc/oil.nvim',
+  -- Optional dependencies
+  dependencies = { { 'echasnovski/mini.icons', opts = {} } },
+  -- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if prefer nvim-web-devicons
+  keys = {
+    {
+      '<leader>o',
+      ':Oil --float<Return>',
+      key_options({ desc = 'Open parent directory' }),
+    },
+  },
+}
+
+function M.config()
+  local detail = false
+
+  require('oil').setup({
+
+    keymaps = {
+      ['gd'] = {
+        desc = 'Toggle file detail view',
+        callback = function()
+          detail = not detail
+          if detail then
+            require('oil').set_columns({ 'icon', 'permissions', 'size', 'mtime' })
+          else
+            require('oil').set_columns({ 'icon' })
+          end
+        end,
+      },
+      ['<CR>'] = 'actions.select',
+      ['<C-s>'] = { 'actions.select', opts = { vertical = true }, desc = 'Open the entry in a vertical split' },
+      ['<C-h>'] = { 'actions.select', opts = { horizontal = true }, desc = 'Open the entry in a horizontal split' },
+      ['<C-t>'] = { 'actions.select', opts = { tab = true }, desc = 'Open the entry in new tab' },
+      ['<C-p>'] = 'actions.preview',
+      ['<C-c>'] = 'actions.close',
+      ['<C-l>'] = 'actions.refresh',
+      ['-'] = 'actions.parent',
+      ['_'] = 'actions.open_cwd',
+      ['`'] = 'actions.cd',
+      ['~'] = { 'actions.cd', opts = { scope = 'tab' }, desc = ':tcd to the current oil directory' },
+      ['gs'] = 'actions.change_sort',
+      ['gx'] = 'actions.open_external',
+      ['g.'] = 'actions.toggle_hidden',
+      ['g\\'] = 'actions.toggle_trash',
+    },
+
+    view_options = {
+      show_hidden = true,
+    },
+
+    -- Configuration for the floating window in oil.open_float
+    float = {
+      -- Padding around the floating window
+      padding = 2,
+      max_width = 0,
+      max_height = 0,
+      border = 'rounded',
+      win_options = {
+        winblend = 0,
+      },
+      -- preview_split: Split direction: "auto", "left", "right", "above", "below".
+      preview_split = 'auto',
+      -- This is the config that will be passed to nvim_open_win.
+      -- Change values here to customize the layout
+      override = function(conf)
+        return conf
+      end,
+    },
+
+    -- Configuration for the actions floating preview window
+    preview = {
+      -- Width dimensions can be integers or a float between 0 and 1 (e.g. 0.4 for 40%)
+      -- min_width and max_width can be a single value or a list of mixed integer/float types.
+      -- max_width = {100, 0.8} means "the lesser of 100 columns or 80% of total"
+      max_width = 0.9,
+      -- min_width = {40, 0.4} means "the greater of 40 columns or 40% of total"
+      min_width = { 40, 0.4 },
+      -- optionally define an integer/float for the exact width of the preview window
+      width = nil,
+      -- Height dimensions can be integers or a float between 0 and 1 (e.g. 0.4 for 40%)
+      -- min_height and max_height can be a single value or a list of mixed integer/float types.
+      -- max_height = {80, 0.9} means "the lesser of 80 columns or 90% of total"
+      max_height = 0.9,
+      -- min_height = {5, 0.1} means "the greater of 5 columns or 10% of total"
+      min_height = { 5, 0.1 },
+      -- optionally define an integer/float for the exact height of the preview window
+      height = nil,
+      border = 'rounded',
+      win_options = {
+        winblend = 0,
+      },
+      -- Whether the preview window is automatically updated when the cursor is moved
+      update_on_cursor_moved = true,
+    },
+
+    -- Configuration for the floating progress window
+    progress = {
+      max_width = 0.9,
+      min_width = { 40, 0.4 },
+      width = nil,
+      max_height = { 10, 0.9 },
+      min_height = { 5, 0.1 },
+      height = nil,
+      border = 'rounded',
+      minimized_border = 'none',
+      win_options = {
+        winblend = 0,
+      },
+    },
+
+    -- Configuration for the floating SSH window
+    ssh = {
+      border = 'rounded',
+    },
+    -- Configuration for the floating keymaps help window
+    keymaps_help = {
+      border = 'rounded',
+    },
+  })
+end
+
+return M
