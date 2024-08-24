4b54e0801aa5cf05f46a4bb7771392bf12e4a886
rvjs <renato.valentim@tempest.com.br>
2024-08-23 23:15:31 -0300

    refactor: added telescope commands

--- config/lazyvim/lua/plugins/telescope.lua
+++ config/lazyvim/lua/plugins/telescope.lua
@@ -15,6 +15,16 @@ return {
         ':Telescope file_browser file_browser path=%:p:h=%:p:h<cr>',
         key_options({ desc = 'Browse Files' }),
       },
+      {
+        '<leader>cbs',
+        ':Telescope lsp_document_symbols<Return>',
+        key_options({ noremap = false, silent = false, desc = 'LSP Document Symbols, Buffer' }),
+      },
+      {
+        '<leader>cbf',
+        ':lua require("telescope.builtin").current_buffer_fuzzy_find({ previewer = false })<CR>',
+        key_options({ noremap = false, silent = false, desc = 'LSP Document Find, Buffer' }),
+      },
     },
 
     config = function(_, opts)
