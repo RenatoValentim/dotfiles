315a756ec271310da1b4f76210a9923a209296fe
rvjs <renato.valentim@tempest.com.br>
2024-08-24 23:59:56 -0300

    refactor: customized oil key maps

--- config/lazyvim/lua/plugins/oil-nvim.lua
+++ config/lazyvim/lua/plugins/oil-nvim.lua
@@ -18,7 +18,6 @@ function M.config()
   local detail = false
 
   require('oil').setup({
-
     keymaps = {
       ['gd'] = {
         desc = 'Toggle file detail view',
@@ -32,21 +31,9 @@ function M.config()
         end,
       },
       ['<CR>'] = 'actions.select',
-      ['<C-s>'] = { 'actions.select', opts = { vertical = true }, desc = 'Open the entry in a vertical split' },
-      ['<C-h>'] = { 'actions.select', opts = { horizontal = true }, desc = 'Open the entry in a horizontal split' },
-      ['<C-t>'] = { 'actions.select', opts = { tab = true }, desc = 'Open the entry in new tab' },
-      ['<C-p>'] = 'actions.preview',
-      ['<C-c>'] = 'actions.close',
-      ['<C-l>'] = 'actions.refresh',
-      ['-'] = 'actions.parent',
-      ['_'] = 'actions.open_cwd',
-      ['`'] = 'actions.cd',
-      ['~'] = { 'actions.cd', opts = { scope = 'tab' }, desc = ':tcd to the current oil directory' },
-      ['gs'] = 'actions.change_sort',
-      ['gx'] = 'actions.open_external',
-      ['g.'] = 'actions.toggle_hidden',
-      ['g\\'] = 'actions.toggle_trash',
     },
+
+    use_default_keymaps = false,
 
     view_options = {
       show_hidden = true,
