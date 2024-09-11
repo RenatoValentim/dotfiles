b8ee579bac62370f0c90d9eb6d263e31bedcfb36
rvjs <renato.valentim@tempest.com.br>
2024-09-11 10:34:26 -0300

    refactor(lazyvim): update mini-animate config

---
+++ config/lazyvim/lua/plugins/mini-animate.lua
@@ -0,0 +1,16 @@
+return {
+  'echasnovski/mini.animate',
+  version = false,
+  event = 'VeryLazy',
+  opts = function(_, opts)
+    opts.scroll = {
+      enable = false,
+    }
+    opts.open = {
+     enable = false
+    }
+    opts.close = {
+     enable = false
+    }
+end,
+}
